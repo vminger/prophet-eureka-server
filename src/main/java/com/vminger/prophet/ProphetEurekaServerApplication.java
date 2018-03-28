2cd888391e2707d21aa52b6e32e0a537883cead4
vminger <vminger@163.com>
2018-03-28 20:43:17 +0800

    Prophet Eureka Server

---
+++ src/main/java/com/vminger/prophet/ProphetEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.vminger.prophet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProphetEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProphetEurekaServerApplication.class, args);
+	}
+}

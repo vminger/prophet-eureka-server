8359881fda9f9b61826ef231528ea1cd459b055d
vminger <vminger@163.com>
2018-03-28 21:16:18 +0800

    Add configure for eureka server.

--- src/main/java/com/vminger/prophet/ProphetEurekaServerApplication.java
+++ src/main/java/com/vminger/prophet/ProphetEurekaServerApplication.java
@@ -2,10 +2,11 @@ package com.vminger.prophet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ProphetEurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProphetEurekaServerApplication.class, args);
 	}
